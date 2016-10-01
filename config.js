b3499ed56b100ff874e98da6fc56991cda13512a
Chris Carrassi <ccarrassi@gmail.com>
2016-10-01 13:49:22 -0700

    let's try this

---
+++ config.js
@@ -0,0 +1,9 @@
+var api = {
+	key: "051754585c0657ff1ced2d08a03322ba",
+	secret: "eb8e38416739ba10",
+	user_id: "137341536@N08",
+	access_token: "72157673630341156-63838b2ded004421",
+	access_token_secret: "245fe206091b98bb"
+};
+
+ module.exports = api;
